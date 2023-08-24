15ad57333a197f273995e9bc540f9c8ee8e320f1
AngelEsquit <esq23221@uvg.edu.gt>
2023-08-24 13:04:08 -0600

    Update Huesped.java

--- Huesped.java
+++ Huesped.java
@@ -1,3 +1,38 @@
 public class Huesped {
+    private String nombreHuesped;
+    private String email;
+    private int numPersonas;
+    private int nivelHuesped;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
     
+    public void setNivelHuesped(int nivelHuesped) {
+        this.nivelHuesped = nivelHuesped;
+    }
+
+    public int getNivelHuesped() {
+        return nivelHuesped;
+    }
+
+    public void setNombreHuesped(String nombreHuesped) {
+        this.nombreHuesped = nombreHuesped;
+    }
+
+    public String getNombreHuesped() {
+        return nombreHuesped;
+    }
+
+    public void setNumPersonas(int numPersonas) {
+        this.numPersonas = numPersonas;
+    }
+
+    public int getNumPersonas() {
+        return numPersonas;
+    }
 }
