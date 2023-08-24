0adc7e64f1b8f4aa5ffbb63d3de6df16d10da6db
AngelEsquit <esq23221@uvg.edu.gt>
2023-08-24 13:09:45 -0600

    Update Habitacion.java

--- Habitacion.java
+++ Habitacion.java
@@ -1,3 +1,47 @@
 public class Habitacion {
+    private int numHabitacion;
+    private int nivelHabitacion;
+    private int capacidad;
+    private int precioNoche;
+    private Boolean asignada = false;
+
+    public void setAsignada(Boolean asignada) {
+        this.asignada = asignada;
+    }
+
+    public Boolean getAsignada() {
+        return asignada;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setNivelHabitacion(int nivelHabitacion) {
+        this.nivelHabitacion = nivelHabitacion;
+    }
     
+    public int getNivelHabitacion() {
+        return nivelHabitacion;
+    }
+
+    public void setNumHabitacion(int numHabitacion) {
+        this.numHabitacion = numHabitacion;
+    }
+
+    public int getNumHabitacion() {
+        return numHabitacion;
+    }
+
+    public void setPrecioNoche(int precioNoche) {
+        this.precioNoche = precioNoche;
+    }
+
+    public int getPrecioNoche() {
+        return precioNoche;
+    }
 }
